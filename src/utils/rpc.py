63a85eae38df7e64291432a402c50aba93be810c
Tips <tips94988@gmail.com>
2025-12-07 14:05:19 +0100

    Rename RPC class to DiscordRPC and refactor code

--- src/utils/rpc.py
+++ src/utils/rpc.py
@@ -2,54 +2,55 @@ from src import *
 from src.utils.config import get
 from src.utils.files import files
 
-class RPC:
+class DiscordRPC:
     def __init__(self):
-        try:
-            if get.rpc.enabled():
-                if get.rpc.showdata():
-                    smalltext = f'Tokens » {len(files.gettokens())} Proxies » 0'
-                else:
-                    smalltext = f'Tokens » Private | Proxies » Private'
-                self.clientid = '1430609793436876820'
-                self.rpc = Presence(self.clientid)
-                self.rpc.connect()
-                self.rpc.update(
-                    state='discord.gg/spamming',
-                    details='Using G4Raid-lite',
-                    start=int(time.time()),
-                    large_image='smalllogorounded',
-                    large_text='discord.gg/spamming',
-                    small_image='folder',
-                    small_text=smalltext,
-                    buttons=[
-                        {'label': 'Join Discord', 'url': 'https://discord.gg/spamming'},
-                        {'label': 'Get G4Raid for FREE', 'url': 'https://github.com/r3ci/G4Raid'}
-                    ]
-                )
-        except Exception as e:
-            pass
-    
-    def update(self, details):
-        try:
-            if get.rpc.enabled():
-                if get.rpc.showdata():
-                    smalltext = f'Tokens » {len(files.gettokens())} Proxies » 0'
-                else:
-                    smalltext = f'Tokens » Private | Proxies » Private'
-                self.rpc.update(
-                    state=details,
-                    details='Using G4Raid-lite',
-                    start=int(time.time()),
-                    large_image='smalllogorounded',
-                    large_text='discord.gg/spamming',
-                    small_image='folder',
-                    small_text=smalltext,
-                    buttons=[
-                        {'label': 'Join Discord', 'url': 'https://discord.gg/spamming'},
-                        {'label': 'Get G4Raid for FREE', 'url': 'https://github.com/r3ci/G4Raid'}
-                    ]
-                )
-        except Exception as e:
-            pass
+        self.client_id = '1430609793436876820'
+        self.rpc = None
+        self.connected = False
+        self.start_time = int(time.time())
 
-RPC = RPC()
+        if not get.rpc.enabled():
+            return
+
+        self._connect()
+
+    def _connect(self):
+        try:
+            self.rpc = Presence(self.client_id)
+            self.rpc.connect()
+            self.connected = True
+
+            self.update(state="Idling...")
+        except Exception:
+            self.connected = False
+
+    def _get_data_string(self):
+        if get.rpc.showdata():
+            token_count = len(files.gettokens())
+            proxy_count = 0 
+            return f'Tokens » {token_count} | Proxies » {proxy_count}'
+        else:
+            return 'Tokens » Private | Proxies » Private'
+
+    def update(self, state="Idling..."):
+        if not self.connected or not self.rpc:
+            return
+
+        try:
+            self.rpc.update(
+                state=state,
+                details='Using G4Raid-lite',
+                start=self.start_time,
+                large_image='smalllogorounded',
+                large_text='discord.gg/spamming',
+                small_image='folder',
+                small_text=self._get_data_string(),
+                buttons=[
+                    {'label': 'Join Discord', 'url': 'https://discord.gg/spamming'},
+                    {'label': 'Get G4Raid for FREE', 'url': 'https://github.com/r3ci/G4Raid'}
+                ]
+            )
+        except Exception:
+            self.connected = False
+
+RPC = DiscordRPC()
